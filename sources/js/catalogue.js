013955ad3685e67b749a9657277cecf121e734a4
valentin prost <valentinprost56@gmail.com>
2024-09-25 15:28:04 +0200

    Envoi de l'id en plus du reste

--- sources/js/catalogue.js
+++ sources/js/catalogue.js
@@ -33,29 +33,31 @@
 
     //affiche les info de l'album
 
-    console.log("Liste des albums");
-	albums.forEach(album => {
-	    let serie = series.get(album.idSerie);
-	  let  auteur = auteurs.get(album.idAuteur);
-	    console.log(album.titre+" N°"+album.numero+" Série:"+serie.nom+" Auteur:"+auteur.nom);
-	});
+    // console.log("Liste des albums");
+	// albums.forEach(album => {
+	//     let serie = series.get(album.idSerie);
+	//   let  auteur = auteurs.get(album.idAuteur);
+	//     console.log(album.titre+" N°"+album.numero+" Série:"+serie.nom+" Auteur:"+auteur.nom);
+	// });
 
     function afficheCatalogue (){
-        albums.forEach((albums)=>{
+        albums.forEach((albums,id)=>{
             let auteur = auteurs.get(albums.idAuteur)
             let serie = series.get(albums.idSerie)
             let titre = albums.titre;
             let prix = albums.prix;
             let num = albums.numero;
+            //let idAlbums = albums
+//console.log(idAlbums)
 
             let nomfichier = `${serie.nom}-${num}-${titre}`
 
             nomfichier = nomfichier.replace(/'|!|\?|\.|"|:|\$/g, "");
 
             innerCatalogue.innerHTML += `
-            <div class="carte col-6 col-lg-3 col-xl-2 mb-3">
+            <div class="carte col-6 col-lg-3 col-xl-2 mb-3" id="${id}">
             <div class="card h-100 bg-secondary text-white">
-                    <a href="detail_BD.html?auteur=${auteur.nom}&serie=${serie.nom}&prix=${prix}&numero=${num}&titre=${titre}"><img src="images/albumsMini/${nomfichier}.jpg" class="card-img-top rounded mt-2 px-3 " alt="..." id="imageAlbum"></a>
+                    <a href="detail_BD.html?auteur=${auteur.nom}&serie=${serie.nom}&prix=${prix}&numero=${num}&titre=${titre}&id=${id}"><img src="images/albumsMini/${nomfichier}.jpg" class="card-img-top rounded mt-2 px-3 " alt="..." id="imageAlbum"></a>
                     <div class="card-body d-flex flex-column">
                         <h5 class="card-title text-wrap" id="titreAlbum">${serie.nom}-${num}-${titre}</h5>
                         <h6 class="card-subtitle mb-2 text-white" id="auteurAlbum">${auteur.nom}</h6>
